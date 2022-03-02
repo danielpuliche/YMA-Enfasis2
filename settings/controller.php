70388ea6b59f3a2824a07f514f6305abfee30d12
danielpuliche <66581056+danielpuliche@users.noreply.github.com>
2022-03-02 17:40:13 -0500

    Registro Solicitante - Falta validación de datos

--- settings/controller.php
+++ settings/controller.php
@@ -80,6 +80,43 @@ if (isset($_POST['signup_tramitador'])) {
     
 }
 
+// Controlador para crear tramitador
+if (isset($_POST['signup_solicitante'])) {
+
+    $nombres = $_POST['names'];
+    $apellidos = $_POST['lastnames'];
+    $identificacion = $_POST['identificacion'];
+    $telefono = $_POST['phone'];
+    $direccion = $_POST['address'];
+    $correo = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $query = "INSERT INTO usuario(`correo`, `nombres`, `apellidos`, `identificacion`, `password_usuario`) VALUES ('$correo','$nombres','$apellidos','$identificacion','$password')";
+    //Ejecuta la consulta
+
+    try{
+        $resultado = mysqli_query($connection, $query);
+    } catch(Exception){
+    }   
+     
+    if (!$resultado) {
+        header("Location: /views/registroSolicitante.php?mensaje=2");
+    }else{
+        $sql = "SELECT usuario.usuario_id from usuario WHERE usuario.correo = '$correo'";
+        $result = $connection->query($sql);
+        $id_solicitante = $result->fetch_assoc()['usuario_id'];
+        $query = "INSERT INTO solicitante(`usuario_id`,`telefono`,`direccion`) VALUES ('$id_solicitante','$telefono','$direccion')";
+
+        //Ejecuta la consulta
+        $resultado = mysqli_query($connection, $query);
+        if (!$resultado) {
+            header("Location: /views/registroSolicitante.php?mensaje=3");
+        }else{
+            //Con este comando retorna al login
+            header("Location: /views/login.php?mensaje=4");
+        }
+    }    
+    
+}
 
 
 
