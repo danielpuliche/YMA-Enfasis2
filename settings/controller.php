87fd8484ab172e444bf52c41bd1fbcd79c6db3e4
danielpuliche <66581056+danielpuliche@users.noreply.github.com>
2022-03-02 17:29:10 -0500

    Registro tramitador - Falta hacer validación de datos

--- settings/controller.php
+++ settings/controller.php
@@ -7,42 +7,89 @@ mysqli_set_charset($connection, "utf8");
 // Controlador para login
 if (isset($_POST['login'])) {
 
-    $usuario = $_POST['user'];
+    $correo = $_POST['email'];
     $password = $_POST['password'];
 
     try {
-        $stmt = $connection->prepare("SELECT password FROM staff WHERE username = ?");
-        $stmt->bind_param("s", $usuario);
+        $stmt = $connection->prepare("SELECT password_usuario FROM usuario WHERE correo = ?");
+        $stmt->bind_param("s", $correo);
         $stmt->execute();
-        $stmt->bind_result($password_admin); //bind_result retorna los resultados de usar SELECT y permite asignarle en ese retorno las variables que se desean usar
+        $stmt->bind_result($password_gotten); //bind_result retorna los resultados de usar SELECT y permite asignarle en ese retorno las variables que se desean usar
         if ($stmt->affected_rows) {
             $existe = $stmt->fetch();
             if ($existe) {
-                if (password_verify($password, $password_admin)) {
+                if (password_verify($password, $password_gotten)) {
                     session_start();
-                    $_SESSION['usuario'] = $usuario;
+                    $_SESSION['usuario'] = $correo;
                     $stmt->close();
                     $connection->close();
                     header("Location: /index.php");
-                }else{
+                } else {
                     $stmt->close();
                     $connection->close();
                     header("Location: /views/login.php?mensaje=1");
                 }
-            } else{
+            } else {
                 $stmt->close();
                 $connection->close();
-                header("Location: /views/login.php?mensaje=1");
+                header("Location: /views/login.php?mensaje=2");
             }
-        }else{
+        } else {
             $stmt->close();
             $connection->close();
-            header("Location: /views/login.php?mensaje=1");
+            header("Location: /views/login.php?mensaje=3");
         }
     } catch (Exception $e) {
         // echo "Error: " . $e->getMessage();
     }
 }
+
+// Controlador para crear tramitador
+if (isset($_POST['signup_tramitador'])) {
+
+    $nombres = $_POST['names'];
+    $apellidos = $_POST['lastnames'];
+    $identificacion = $_POST['identificacion'];
+    $correo = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $query = "INSERT INTO usuario(`correo`, `nombres`, `apellidos`, `identificacion`, `password_usuario`) VALUES ('$correo','$nombres','$apellidos','$identificacion','$password')";
+    //Ejecuta la consulta
+
+    try{
+        $resultado = mysqli_query($connection, $query);
+    } catch(Exception){
+    }   
+     
+    if (!$resultado) {
+        header("Location: /views/registroTramitador.php?mensaje=2");
+    }else{
+        $sql = "SELECT usuario.usuario_id from usuario WHERE usuario.correo = '$correo'";
+        $result = $connection->query($sql);
+        $id_tramitador = $result->fetch_assoc()['usuario_id'];
+        $query = "INSERT INTO tramitador(`usuario_id`) VALUES ('$id_tramitador')";
+
+        //Ejecuta la consulta
+        $resultado = mysqli_query($connection, $query);
+        if (!$resultado) {
+            header("Location: /views/registroTramitador.php?mensaje=3");
+        }else{
+            //Con este comando retorna al login
+            header("Location: /views/login.php?mensaje=4");
+        }
+    }    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // Controlador para editar film
 if (isset($_POST['edit-film'])) {
@@ -52,29 +99,11 @@ if (isset($_POST['edit-film'])) {
     $year = $_POST['year'];
     $language = $_POST['language'];
     $duration = $_POST['duration'];
-    
+
     $query = "UPDATE film f SET f.description='$description', f.release_year=$year, f.language_id=$language, f.rental_duration=$duration WHERE f.film_id = $id";
     //Ejecuta la consulta
     $resultado = mysqli_query($connection, $query);
-    if(!$resultado){
-        header("Location: /views/read.php?mensaje=2");
-    }
-    //Con este comando retorna a la página principal y muestra los datos
-    header("Location: /views/read.php?mensaje=1");
-}
-
-// Controlador para crear film
-if (isset($_POST['agregar-film'])) {
-        
-    $titulo = $_POST['titulo'];
-    $descripcion = $_POST['description'];
-    $year = $_POST['year'];
-    $lenguaje = $_POST['language'];
-    $duration = $_POST['duration'];
-    $query = "INSERT INTO film (title, description, release_year, language_id, rental_duration) VALUES ('$titulo', '$descripcion', $year ,$lenguaje, $duration)";
-    //Ejecuta la consulta
-    $resultado = mysqli_query($connection, $query);
-    if(!$resultado){
+    if (!$resultado) {
         header("Location: /views/read.php?mensaje=2");
     }
     //Con este comando retorna a la página principal y muestra los datos
@@ -86,10 +115,8 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $query = "DELETE FROM film WHERE film_id = $id";
     $resultado = mysqli_query($connection, $query);
-    if(!$resultado){
+    if (!$resultado) {
         header("Location: /views/read.php?mensaje=2");
     }
     header("Location: /views/read.php?mensaje=1");
 }
-
-?>
