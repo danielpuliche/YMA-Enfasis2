863591c912225f100903d6dc96365cb5f4ac3780
danielpuliche <66581056+danielpuliche@users.noreply.github.com>
2022-03-09 18:07:35 -0500

    Validar caracteres en front y campos vacíos en back

---
+++ static/js/validarInputs.js
@@ -0,0 +1,10 @@
+function ValChar(string) { //solo letras y numeros
+    var out = '';
+    var filtro = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890@.-_'; //Caracteres validos
+
+    for (var i = 0; i < string.length; i++)
+        if (filtro.indexOf(string.charAt(i)) != -1)
+            out += string.charAt(i);
+    return out;
+}
+
