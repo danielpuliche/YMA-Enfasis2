4f3f20a22aafdc30257000708b0ac029c29d9188
Jhon Fredy Romero <jhonrom@unicauca.edu.co>
2022-03-09 23:31:08 -0500

    Negociar tramite formulario funcional

--- views/tramitador/negociarTramite.php
+++ views/tramitador/negociarTramite.php
@@ -26,35 +26,61 @@ include('../../settings/db.php');
     <!-- Main content -->
     <section class="content">
     <form class="p-4" method="POST" action="/settings/solicitante/controller.php">
-
+        <?php
+        try {
+            $conn = mysqli_connect($host, $user, $pw, $db);
+            $conn->set_charset("utf8");
+            $id = $_GET['id'];
+            $tipo = $_GET['tipo'];
+            if ($tipo==0) {
+                //0 = cita medica
+                $sql = "SELECT u.nombres, u.apellidos, t.eps, t.regimen, tm.especialidad, tm.fecha_disponible, t.precio FROM tramite t
+                INNER JOIN usuario u ON u.usuario_id=t.solicitante_id
+                INNER JOIN tramite_citamedica tm ON tm.tramite_citamedica_id=t.tramite_id
+                WHERE t.tramite_id=$id";
+            }else{
+                //1 = medicamento
+                $sql = "SELECT u.nombres, u.apellidos, t.eps, t.regimen, tm.medicamentos, tm.fecha_entrega, t.precio FROM tramite t
+                INNER JOIN usuario u ON u.usuario_id=t.solicitante_id
+                INNER JOIN tramite_medicamentos tm ON tm.tramite_medicamentos_id=t.tramite_id
+                WHERE t.tramite_id=$id";
+            }
+            $resultado = $conn->query($sql);
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+            echo $error;
+        }
+        $tramite = $resultado->fetch_assoc()
+        ?>
         <div class="form-row">
             <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>Solicitante:  <?php echo $tramite['nombres']; ?> <?php echo $tramite['apellidos']; ?></h4>
             </div>
             <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>Tipo de trámite: <?php if($tipo==0){echo 'Cita Médica';}else{echo 'Solicitud Medicamentos';}?></h4>
             </div>
         </div>
         <div class="form-row">
         <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>EPS: <?php echo $tramite['eps']; ?></h4>
             </div>
             <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>Régimen: <?php echo $tramite['regimen']; ?></h4>
             </div>
         </div>
         <div class="form-row">
         <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>Especialidad: <?php echo $tramite['especialidad']; ?></h4>
             </div>
             <div class="form-group col-md-6">
-                <h3>Solicitante</h3>
+                <h4>Fecha para programación: <?php echo $tramite['fecha_disponible']; ?></h4>
             </div>
         </div>
     </form>
         <div class="d-flex justify-content-around align-items-center">
             <img src="../../static/img/down.png" id="down" width="100em" alt="">
-            <input style="font-size: 6em; width: 3em; border: 0px solid; color:black;" type="number" name="" id="precio" value="8000" disabled>
+            <span id="val-min" style="display: none;"><?php echo $tramite['precio']; ?></span>
+            <input style="font-size: 6em; width: 3em; border: 0px solid; color:black;" type="number" name="precio" id="precio" value="<?php echo $tramite['precio']; ?>"  min="<?php echo $tramite['precio']; ?>" disabled required>
             <img src="../../static/img/up.png" id="up" width="100em" alt="">
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -64,7 +90,10 @@ include('../../settings/db.php');
                     $('#precio').val(parseInt($('#precio').val()) + 500);
                 });
                 $('#down').click(()=>{
-                    $('#precio').val(parseInt($('#precio').val()) - 500);
+                    let valMin = $('#val-min').html()
+                    if(parseInt($('#precio').val())>parseInt(valMin)){
+                        $('#precio').val(parseInt($('#precio').val()) - 500);
+                    }
                 });
             });
         </script>
