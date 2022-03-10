c93760df2c52174f96ab7da9a70ea3d98d133e27
danielpuliche <66581056+danielpuliche@users.noreply.github.com>
2022-03-09 19:00:03 -0500

    Re direcciones menu lateral

--- templates/menu.php
+++ templates/menu.php
@@ -1,30 +1,34 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    
+
+    <?php
+    $tipo = $GLOBALS['mensaje'];
+    ?>
+
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="/views/<?php echo strtolower($tipo) ?>/<?php echo strtolower($tipo) ?>Main.php" class="brand-link">
         <img src="https://iconarchive.com/download/i109565/cjdowner/cryptocurrency-flat/ICON-ICX.ico" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        
+
         <span class="brand-text font-weight-light">
             <?php
-                echo $GLOBALS['mensaje'];
+            echo $tipo;
             ?>
-        </span> 
-    
+        </span>
+
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
-        
+
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="info">
                 <a href=# class="d-block">
                     <?php
-                        echo ($_SESSION['usuario']);
+                    echo ($_SESSION['usuario']);
                     ?>
-                </a> 
+                </a>
             </div>
-          </div>      
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -33,7 +37,7 @@
                with font-awesome or any other icon font library -->
 
                 <li class="nav-item">
-                <a href="#" class="nav-link">
+                    <a href="/views/<?php echo strtolower($tipo) ?>/<?php echo strtolower($tipo) ?>Main.php" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
                         <p>Inicio</p>
                     </a>
